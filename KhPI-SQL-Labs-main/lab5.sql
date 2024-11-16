6bd7f54172311e5dd5845e487138f42e901cc94c
7toli <serega.brezhcko@gmail.com>
2024-11-17 01:29:04 +0200

    lab5

--- KhPI-SQL-Labs-main/lab5.sql
+++ KhPI-SQL-Labs-main/lab5.sql
@@ -1,7 +1,6 @@
 -- 1
 select region, sum(population) as pop_region
 from cities
-where region in ('N', 'S', 'E', 'W', 'C')
 group by region;
 
 -- 2
@@ -13,19 +12,18 @@ having count(name) >= 10;
 -- 3
 select name, population
 from cities
-where region in
-   (select region
-   from cities
-   group by region
-   having count(*) >= 5)
+where region in (
+   select uuid
+   from regions
+   where area_quantity >= 5)
 order by population desc
 limit 5 offset 10;
 
 -- 4
 select region, sum(population) as pop_region
 from cities
-where name in
-   (select name
+where name in (
+   select name
    from cities
    where population > 300000)
 group by region;
@@ -33,9 +31,8 @@ group by region;
 -- 5
 select name, population
 from cities
-where region in
-   (select region
-   from cities
-   grounp by region
-   having count(name) <= 5)
+where region in (
+   select uuid
+   from regions
+   where area_quantity <= 5)
 and (population < 150000 or population > 500000);
