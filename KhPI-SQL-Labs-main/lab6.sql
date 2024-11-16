e20d775221a75bf18dda526b5f9adaf145affb6d
7toli <serega.brezhcko@gmail.com>
2024-11-17 01:34:36 +0200

    lab6

---
+++ KhPI-SQL-Labs-main/lab6.sql
@@ -0,0 +1,9 @@
+-- 1
+select cities.name as Cities, regions.name as Regions
+from cities join regions on cities.region =
+regions.uuid where population > 350000;
+
+-- 2
+select cities.name as NordRegion
+from cities join regions on cities.region =
+regions.uuid where regions.name = 'Nord';
--- lab6.sql
+++
@@ -1,7 +0,0 @@
-1) select cities.name as Cities, regions.name as Regions
-from cities join regions on cities.region =
-regions.uuid where population > 35000;
-
-2) select cities.name as NordRegion
-from cities join regions on cities.region =
-regions.uuid where regions.name = 'Nord';
